24a5e46028f10db7e9da6cee8b252d0b22ade75f
Abhijeet <srivastava.abhijeet96@gmail.com>
2022-10-13 17:26:09 +1100

    Fixed Changes

--- PetMeNow/src/main/java/com/petmenow/controller/UserController.java
+++ PetMeNow/src/main/java/com/petmenow/controller/UserController.java
@@ -5,11 +5,11 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.petmenow.constants.FailureConstants;
@@ -84,8 +84,8 @@ public class UserController {
 				SuccessConstants.USER_UPDATE_DETAILS_SUCCESS.getSuccessMsg(), response);
 	}
 
-	@GetMapping(value = "/details")
-	public ResponseEntity<Object> getUserDetails(@RequestParam(name = "id", required = true) Long id)
+	@GetMapping(value = "/details/id")
+	public ResponseEntity<Object> getUserDetails(@PathVariable Long id)
 			throws CommonException {
 		Object response = userService.getUserDetails(id);
 		if (response instanceof Integer) {
